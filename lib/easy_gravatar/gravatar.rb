2222e8f91bd968e2d41fc0dac4cf272a4f8a6f1f
Dennis van de Hoef <dennisvdhoef@gmail.com>
2015-02-08 13:05:40 +0100

    add basic functionality to gravatar

--- lib/easy_gravatar/gravatar.rb
+++ lib/easy_gravatar/gravatar.rb
@@ -1,11 +1,25 @@
+require 'digest/md5'
+require 'net/http'
+require 'uri'
+
 module EasyGravatar
   class Gravatar
 
-    def initialize(email)
+    attr_reader :md5
 
+    def initialize(email)
+     @md5 = Digest::MD5.hexdigest(email.downcase)
+    end
+
+    def hash
+      @hash ||= get_hash
     end
 
     private
 
+    def get_hash
+      url = "https://www.gravatar.com/#{@md5}.json"
+      Net::HTTP.get(URI.parse(url))
+    end
   end
 end
--- spec/easy_gravatar/gravatar_spec.rb
+++ spec/easy_gravatar/gravatar_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
 describe EasyGravatar::Gravatar do
-  pending "write it"
 
   before(:each) do
     @gravatar_user = EasyGravatar::Gravatar.new('foo@bar.com')
+  end
+
+  it 'can make a vallid md5 hash' do
+    expect(@gravatar_user.md5).to eq('f3ada405ce890b6f8204094deb12d8a8')
   end
 
 end
